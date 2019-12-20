50885020fb8d6504cf23064160420761189d47a7
Jonghyun-K1m <37470085+Jonghyun-K1m@users.noreply.github.com>
2019-12-20 16:57:35 +0900

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -11,33 +11,31 @@ int main() {
 	vector<vector<string>> tc;
 	/*v.push_back(10);
 	
-	for (auto i = v.begin(); i != v.end(); ++i)
-		cout << *i;
-		*/
-	int size = 4;
+
+	/*int size = 4;
 	for (int i = 0; i < 5; i++) {
 			vector<int> a(size);
-
 			v.push_back(a);
 	}
 	for (auto& j : v) {
 		for (auto& k : j)
 			cout << k;
 		cout << endl;
-	}
+	}*/
+	int size = 4;
+	vector<string> input = { "inc","kor" };
+	
 	for (int i = 0; i < 5; i++) {
-		string n = "in";
-		vector<string> a(i);
-		a.push_back(n);
-		tc.push_back(a);
-	}
-	/*string a;
-	a = "sd";
-	vector<string> k;
-	k.push_back(a);*/
-	for (auto& j : tc) {
+		string n = "in";	
+		//vector<string> a(size);
+		//a.push_back(n);
+		tc.push_back(input);
+	} 
+
+	for (auto& j : tc) {	
 		for (auto& k : j)
 			cout << k;
+		cout << endl;
 	}
 
 }
