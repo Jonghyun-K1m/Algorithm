af5ee89cdba8ac548cda8e08b0914ea57bd3d204
Jonghyun-K1m <37470085+Jonghyun-K1m@users.noreply.github.com>
2019-12-26 16:20:13 +0900

    Create hash.cpp

---
+++ programmers/hash.cpp
@@ -0,0 +1,31 @@
+/*
+https://programmers.co.kr/learn/courses/30/lessons/42578
+
+
+*/
+
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    map<string,int> m;
+    map<string,int>::iterator it;
+    
+    vector<string> temp;
+    
+    for(auto& i:clothes){
+        
+        m[i[1]]++;
+        
+    }
+    for(it=m.begin();it!=m.end();it++){
+        //cout<<it->first<<it->second<<endl;
+        answer=answer*(it->second+1);
+    }
+    return answer-1;
+}
